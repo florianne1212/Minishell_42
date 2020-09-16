64dd1d70061dcf61dd449a63407e0b2df5034b2a
lcoif42 <lcoiffie@student.42.fr>
2020-09-16 17:09:20 +0200

    cd modifie, de meme que les retours de cd, je crois que ca fonctionne

--- srcs/builtin_cd.c
+++ srcs/builtin_cd.c
@@ -6,7 +6,7 @@
 /*   By: lcoiffie <lcoiffie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/12 16:45:42 by lcoiffie          #+#    #+#             */
-/*   Updated: 2020/09/13 15:48:33 by lcoiffie         ###   ########.fr       */
+/*   Updated: 2020/09/16 17:06:25 by lcoiffie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,17 +45,19 @@ int			cd_home(t_list_env **env)
 	int		ret;
 
 	ret = 0;
-	oldpath = getcwd(NULL, 0);
+	oldpath = ft_getenv(*env, "PWD");
 	newpath = ft_getenv(*env, "HOME");
-	if (!oldpath || !newpath)
-		return (cd_error(NULL, 1, oldpath, newpath));
+	if (!newpath)
+	{
+		ft_putendl_fd("minishell: cd: \" HOME \" non defini", 2);
+		return (1);
+	}
 	if (chdir(newpath))
 		return (cd_error(newpath, 1, oldpath, newpath));
-	if ((ft_setenv(env, "OLDPWD", oldpath, 1)) == -1)
-		ret = 1;
-	free(oldpath);
-	if ((ft_setenv(env, "PWD", newpath, 1)) == -1)
-		ret = 1;
+	ft_setenv(env, "OLDPWD", oldpath, 1);
+	if (oldpath)
+		free(oldpath);
+	ft_setenv(env, "PWD", newpath, 1);
 	free(newpath);
 	return (ret);
 }
@@ -72,23 +74,20 @@ int			cd_back(t_list_env **env, int fd)
 	int		ret;
 
 	ret = 0;
-	oldpath = getcwd(NULL, 0);
+	oldpath = ft_getenv(*env, "PWD");
 	newpath = ft_getenv(*env, "OLDPWD");
 	if (!newpath)
 	{
 		ft_putendl_fd("minishell: cd: \" OLDPWD \" non defini", 2);
 		return (1);
 	}
-	if (!oldpath)
-		return (cd_error(NULL, 1, oldpath, newpath));
 	if (chdir(newpath))
 		return (cd_error(newpath, 1, oldpath, newpath));
-	if ((ft_setenv(env, "PWD", newpath, 1)) == -1)
-		ret = 1;
-	if ((ft_setenv(env, "OLDPWD", oldpath, 1)) == -1)
-		ret = 1;
+	ft_setenv(env, "PWD", newpath, 1);
+	ft_setenv(env, "OLDPWD", oldpath, 1);
 	ft_putendl_fd(newpath, fd);
-	free(oldpath);
+	if (oldpath)
+		free(oldpath);
 	free(newpath);
 	return (ret);
 }
@@ -109,11 +108,10 @@ int			cd_abs_path(t_list_env **env, char *newpath, char *arg_path)
 		return (cd_error(NULL, 1, oldpath, NULL));
 	if (chdir(newpath))
 		return (cd_error(arg_path, 1, oldpath, NULL));
-	if ((ft_setenv(env, "OLDPWD", oldpath, 1)) == -1)
-		ret = 1;
-	free(oldpath);
-	if ((ft_setenv(env, "PWD", newpath, 1)) == -1)
-		ret = 1;
+	ft_setenv(env, "OLDPWD", oldpath, 1);
+	if (oldpath)
+		free(oldpath);
+	ft_setenv(env, "PWD", newpath, 1);
 	return (ret);
 }
 
